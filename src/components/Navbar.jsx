c0c862f8675b23279029666766b480e8425a67d2
Reyad-Hoq <rafiulhoq40@gmail.com>
2026-04-10 02:25:00 +0600

    nav bar added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="shadow">
+      <div className="navbar bg-base-100 w-9/12 mx-auto">
+        <div className="flex-1">
+          <a className="bg-linear-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent text-3xl font-bold">DigiTools</a>
+        </div>
+        <ul className="menu menu-horizontal px-1">
+          <li><a>Products</a></li>
+          <li><a>Features</a></li>
+          <li><a>Pricing</a></li>
+          <li><a>Testimonials</a></li>
+          <li><a>FAQ</a></li>
+        </ul>
+        <div className="flex-none">
+          <div className="dropdown dropdown-end">
+            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+              <div className="indicator">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /> </svg>
+              </div>
+            </div>
+            <div
+              tabIndex={0}
+              className="card card-compact dropdown-content bg-base-100 z-1 mt-3 w-52 shadow">
+              <div className="card-body">
+                <span className="text-lg font-bold">8 Items</span>
+                <span className="text-info">Subtotal: $999</span>
+                <div className="card-actions">
+                  <button className="btn btn-primary btn-block">View cart</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <button className="btn btn-ghost mx-2">Log In</button>
+          <button className="btn bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-full text-white">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
