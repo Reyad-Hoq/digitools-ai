d5bef095da3de34bc824bb042f01023c15efbad5
Reyad-Hoq <rafiulhoq40@gmail.com>
2026-04-10 21:02:52 +0600

    button added in banner

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -3,18 +3,20 @@ import bannerImg from "../assets/banner.png";
 const Banner = () => {
   return (
     <div className="hero bg-base-100 min-h-screen w-9/12 mx-auto">
-      <div className="hero-content flex-col lg:flex-row-reverse">
+      <div className="hero-content flex-col lg:flex-row-reverse text-center lg:text-left gap-16">
         <img
           src={bannerImg}
           className="max-w-sm rounded-lg shadow-2xl"
         />
         <div>
-          <h1 className="text-5xl font-bold">Supercharge YourDigital Workflow</h1>
+          <h1 className="text-5xl font-bold">Supercharge Your <br />Digital Workflow</h1>
           <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-            quasi. In deleniti eaque aut repudiandae et a id nisi.
+            Access premium AI tools, design assets, templates, and productivity
+            software—all in one place. Start creating faster today.
+            Explore Products
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <button className="btn bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-full text-white mx-2 hover:scale-103 delay-100">Explore Products</button>
+          <button className="btn border-2 border-gradient-to-r from-[#4F39F6] to-[#9514FA] rounded-full text-white">Watch Demo</button>
         </div>
       </div>
     </div>
