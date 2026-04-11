7a463179df9a8262b1bb70534a6e97f6911905ff
Reyad-Hoq <rafiulhoq40@gmail.com>
2026-04-11 14:23:03 +0600

    features and tagtype updated with color

--- src/products/Availableproduct.jsx
+++ src/products/Availableproduct.jsx
@@ -6,25 +6,17 @@ const Availableproduct = ({ products }) => {
       {
         products.map((product) => {
           console.log(product);
-          return <div className="card w-full bg-base-100 shadow-sm">
+          return <div className="card w-full bg-base-100 shadow-lg">
             <div className="card-body">
-              <span className="badge badge-xs badge-warning">{product.tag}</span>
+              <span className={`badge badge-lg badge-soft text-sm py-1 px-2 ${product.tag === "popular" ? "badge-primary" : product.tag === "new" ? "badge-success" : "badge-warning "}`}>{product.tag}</span>
               <h2 className="text-3xl font-bold">{product.name}</h2>
-              <p className="text-lg font-semibold text-gray-500">${product.description}</p>
-              <span className="text-xl">${product.price}/mo</span>
+              <p className="text-lg font-semibold text-gray-500">{product.description}</p>
+              <span className="text-xl">${product.price}/{product.period === "monthly" ? "mo" : product.period === "yearly" ? "yr" : "one-time"}</span>
               <ul className="mt-6 flex flex-col gap-2 text-xs">
-                <li>
+                {product.features.map(feature => <li>
                   <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-                  <span>High-resolution image generation</span>
-                </li>
-                <li>
-                  <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-                  <span>Customizable style templates</span>
-                </li>
-                <li>
-                  <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-                  <span>Batch processing capabilities</span>
-                </li>
+                  <span>{feature}</span>
+                </li>)}
               </ul>
               <div className="mt-6">
                 <button className="btn btn-block bg-linear-to-r from-[#4F39F6] to-[#9514FA] rounded-full text-white">Buy Now</button>
