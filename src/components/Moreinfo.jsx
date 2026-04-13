4d458f0d66f4ff36065c5428c51c28adc4d4317c
Reyad-Hoq <rafiulhoq40@gmail.com>
2026-04-13 23:22:43 +0600

    footer and content updated for different screen

--- src/components/Moreinfo.jsx
+++ src/components/Moreinfo.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+
 const Moreinfo = () => {
   return (
     <div className="bg-linear-to-r from-[#4F39F6] to-[#9514FA] w-full mx-auto flex justify-center items-center flex-col text-center py-30 space-y-6 text-white">
-      <h1>Ready to Transform Your Workflow?</h1>
+      <h1 className="text-4xl font-bold">Ready to Transform Your Workflow?</h1>
       <p>Join thousands of professionals who are already using Digitools to work smarter. <br /> Start your free trial today.</p>
       <div className="mt-6 flex gap-4">
         <button className="btn "><span className="bg-linear-to-r from-[#4F39F6] to-[#9514FA] bg-clip-text text-transparent">Explore Products</span></button>
         <button className="btn bg-transparent"><span className="bg-white bg-clip-text text-transparent ">Watch Demo</span></button>
-
       </div>
+      <p>14-day free trial • No credit card required • Cancel anytime</p>
+
     </div >
   )
 };
